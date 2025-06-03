e53100459e2f974282e5230a6a1a930177e2cfee
ayanmondalXTZ <ayanmondal741181@gmail.com>
2025-06-03 14:27:21 +0530

    any message

--- console.js
+++ console.js
@@ -1,2 +1,8 @@
-console.log("Ayan Mondal");
+console.log("Ayan Mondal"); // 	General messages
 console.log("Suman Mondal");
+console.error("ayan mondal");//	Errors
+console.warn("plz correct your age") //Warnings
+console.info("hello"); //	Informational messages
+// console.clear(); clear the console 
+let fruits = ["Apple", "Banana", "Mango"];
+console.table(fruits); // show data in a table formate use for array and object
