02d42557d2f0268c3bcce6b7304ef9221f3e25c0
ayanmondalXTZ <ayanmondal741181@gmail.com>
2025-06-03 15:52:32 +0530

    any message

--- Data_Type.js
+++ Data_Type.js
@@ -9,3 +9,40 @@ let x = 10;       // x is a number
 x = "Ayan";       // now x is a string
 x = true;         // now x is a boolean
 console.log(x); // Out put is : true
+
+// let a = 10;
+// let a = 20;
+// console.log(a); this is send a error because you changed the variable value but could't declare same variable !
+________________________________________________________________________________
+// Key Difference Example !
+// Primitive (copied by value)
+let a = 10;
+let y = a;
+y = 20;
+console.log(a); // 10 (unchanged)
+
+// Non-Primitive (copied by reference)
+let arr1 = [1, 2];
+let arr2 = arr1;
+arr2.push(3);
+console.log(arr1); // [1, 2, 3] (modified)
+
+
+_______________________________________________________________
+
+// all primitive data type
+// Examples:
+
+// string ("hello")
+
+// number (42, 3.14)
+
+// boolean (true, false)
+
+// null (intentional absence of value)
+
+// undefined (uninitialized variable)
+
+// symbol (unique identifier, Symbol('id'))
+
+// bigint (large integers, 9007199254740991n)
---
+++ Object.js
@@ -0,0 +1,7 @@
+let studentInfo = {
+    name: "ayan mondal",
+    age: 18,
+    roll: 16,
+};
+console.log(studentInfo.age)
+console.log(`My Name is ${studentInfo.name} My Age is ${studentInfo.age} and my roll Number is ${studentInfo.roll}`)
